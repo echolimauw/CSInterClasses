3637f649f25b00b506faf2c6b0c6978db9b80b13
Evan Leahy <49353272+echolimauw@users.noreply.github.com>
2021-04-27 01:34:15 -0700

    Cleaner/Crisper methods
    Working on better practices writing methods

---
+++ CSIntermediateClasses/CSIntermediateClasses/Point.cs
@@ -0,0 +1,27 @@
+﻿namespace CSIntermediateClasses
+{
+    partial class Program
+    {
+        public class Point
+        {
+            public int X;
+            public int Y;
+
+            public Point(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+            public void Move(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+            public void Move(Point newLocation)
+            {
+                this.X = newLocation.X;   
+                this.Y = newLocation.Y;
+            }
+        }
+    }
+}
--- CSIntermediateClasses/CSIntermediateClasses/Program.cs
+++ CSIntermediateClasses/CSIntermediateClasses/Program.cs
@@ -6,9 +6,14 @@ namespace CSIntermediateClasses
     {
         static void Main(string[] args)
         {
-            var customer = new Customer(1, "Jim");
+            var point = new Point(15, 30);
+            point.Move(new Point(45, 75));
+            Console.WriteLine("Point is at ({0}, {1}).", point.X, point.Y);
+            
+            /*var customer = new Customer(1, "Jim");
             Console.WriteLine(customer.Id);
-            Console.WriteLine(customer.Name);
+            Console.WriteLine(customer.Name);*/
+
         }
     }
 }
