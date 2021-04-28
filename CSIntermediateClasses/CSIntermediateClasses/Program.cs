f9831169208e9888bb0d0ec17d0004421617255b
Evan Leahy <49353272+echolimauw@users.noreply.github.com>
2021-04-28 14:47:33 -0700

    Try/Catch
    Added Try/Catch cases to help with exception handling.

--- CSIntermediateClasses/CSIntermediateClasses/Program.cs
+++ CSIntermediateClasses/CSIntermediateClasses/Program.cs
@@ -6,10 +6,22 @@ namespace CSIntermediateClasses
     {
         static void Main(string[] args)
         {
-            var point = new Point(15, 30);
-            point.Move(new Point(45, 75));
-            Console.WriteLine("Point is at ({0}, {1}).", point.X, point.Y);
             
+        }
+
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(15, 30);
+                point.Move(new Point(45, 75));
+                Console.WriteLine("Point is at ({0}, {1}).", point.X, point.Y);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error occurred.");
+            }
+
             /*var customer = new Customer(1, "Jim");
             Console.WriteLine(customer.Id);
             Console.WriteLine(customer.Name);*/
