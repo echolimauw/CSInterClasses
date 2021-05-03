b636284000aee1c7c368b3cbbf9b260491492722
Evan Leahy <49353272+echolimauw@users.noreply.github.com>
2021-05-03 08:51:34 -0700

    intro to indexers
    initial practice with indexers

--- CSIntermediateClasses/CSIntermediateClasses/Program.cs
+++ CSIntermediateClasses/CSIntermediateClasses/Program.cs
@@ -5,35 +5,10 @@
     {
         static void Main(string[] args)
         {
-            var person = new Person(new System.DateTime(1987, 1, 22));
-            System.Console.WriteLine(person.Age);
+            var cookie = new HttpCookie();
+            cookie["name"] = "Jim";
+
+            System.Console.WriteLine(cookie["name"]);
         }
-
-        /*static void UseParams()
-        {
-            var calculator = new Calculator();
-            Console.WriteLine(calculator.Add(1, 2));
-            Console.WriteLine(calculator.Add(1, 2, 3));
-            Console.WriteLine(calculator.Add(1, 2, 3, 4));
-        }
-
-        static void UsePoints()
-        {
-            try
-            {
-                var point = new Point(15, 30);
-                point.Move(new Point(45, 75));
-                Console.WriteLine("Point is at ({0}, {1}).", point.X, point.Y);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("An unexpected error occurred.");
-            }
-
-            *//*var customer = new Customer(1, "Jim");
-            Console.WriteLine(customer.Id);
-            Console.WriteLine(customer.Name);*//*
-
-        }*/
     }
 }
