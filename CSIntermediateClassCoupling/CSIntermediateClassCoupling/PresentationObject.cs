d931903faceaa6ecb396dfba64ee9368d04b5adf
Evan Leahy <49353272+echolimauw@users.noreply.github.com>
2021-05-04 16:54:41 -0700

    Inheritance refresher
    Reviewed conceptual and syntactical rules for inheritance.

---
+++ CSIntermediateClassCoupling/CSIntermediateClassCoupling/PresentationObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSIntermediateClassCoupling
+{
+    partial class Program
+    {
+        public class PresentationObject
+        {
+            public int Width { get; set; }
+            public int Height { get; set; }
+
+            public void Copy()
+            {
+                Console.WriteLine("Object copied to clipboard.");
+            }public void Duplicate()
+            {
+                Console.WriteLine("Object duplicated.");
+            }
+        }
+    }
+}
---
+++ CSIntermediateClassCoupling/CSIntermediateClassCoupling/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSIntermediateClassCoupling
+{
+    partial class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            text.Width = 100;
+            text.Copy();
+
+        }
+    }
+}
